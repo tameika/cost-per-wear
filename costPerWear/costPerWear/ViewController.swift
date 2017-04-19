e8332c34c9c5696171c6f318e0ec1ed999700866
tameika <tameika.lawrence@gmail.com>
2017-04-19 02:50:19 -0400

    new branch

---
+++ costPerWear/costPerWear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 4/19/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
