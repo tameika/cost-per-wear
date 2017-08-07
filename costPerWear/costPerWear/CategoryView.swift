8abf111dd58b39dea5cc24c36ae49a3be821a9ad
tameika <tameika.lawrence@gmail.com>
2017-08-07 01:29:22 -0400

    buttons

--- costPerWear/costPerWear/CategoryView.swift
+++ costPerWear/costPerWear/CategoryView.swift
@@ -11,20 +11,58 @@ import UIKit
 import SnapKit
 
 
-class CategoryView {
+class CategoryView: UIView {
     
     var topsBtn: UIButton!
     var bottomsBtn: UIButton!
     var dressesBtn: UIButton!
     var shoesBtn: UIButton!
     
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
-
-    
+    override init(frame: CGRect){
+        super.init(frame: frame)
         
+        
+        topsBtn = UIButton(frame: CGRect.zero)
+        topsBtn.translatesAutoresizingMaskIntoConstraints = false
+        topsBtn.backgroundColor = UIColor.bloodOrange
+        topsBtn.setTitle("Tops", for: .normal)
+        //addTarget
+        addSubview(topsBtn)
+        
+        
+        bottomsBtn = UIButton(frame: CGRect.zero)
+        bottomsBtn.translatesAutoresizingMaskIntoConstraints = false
+        bottomsBtn.backgroundColor = UIColor.bloodOrange
+        bottomsBtn.setTitle("Bottoms", for: .normal)
+         //addTarget
+        addSubview(bottomsBtn)
+        
+        
+        dressesBtn = UIButton(frame: CGRect.zero)
+        dressesBtn.translatesAutoresizingMaskIntoConstraints = false
+        dressesBtn.backgroundColor = UIColor.bloodOrange
+        dressesBtn.setTitle("Dresses", for: .normal)
+         //addTarget
+        addSubview(dressesBtn)
+        
+        
+        shoesBtn = UIButton(frame: CGRect.zero)
+        shoesBtn.translatesAutoresizingMaskIntoConstraints = false
+        shoesBtn.backgroundColor = UIColor.bloodOrange
+        shoesBtn.setTitle("Shoes", for: .normal)
+         //addTarget
+        addSubview(shoesBtn)
+        
+        
+        
+        
+        
+    }
+    
+    
 }
