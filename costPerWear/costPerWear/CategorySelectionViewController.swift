03b8c82197c9164cc62395e0009f7e280229ed4e
tameika <tameika.lawrence@gmail.com>
2017-08-07 01:07:20 -0400

    categoryVC + view added

---
+++ costPerWear/costPerWear/CategorySelectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategorySelectionViewController.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 8/7/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class CategorySelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    }
+
+    
+}
---
+++ costPerWear/costPerWear/CategoryView.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryView.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 8/7/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+
+class CategoryView {
+    
+    var topsBtn: UIButton!
+    var bottomsBtn: UIButton!
+    var dressesBtn: UIButton!
+    var shoesBtn: UIButton!
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+
+    
+        
+}
