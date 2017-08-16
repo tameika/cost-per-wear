339119d5eb1d48fe168e1402d57f59ec2a2dbc07
tameika <tameika.lawrence@gmail.com>
2017-08-15 22:59:24 -0400

    notes

---
+++ costPerWear/costPerWear/TODO.swift
@@ -0,0 +1,25 @@
+//
+//  TODO.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 8/14/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import Foundation
+
+
+/*  MARK : TODO
+ 
+ UI
+ - redo constraints with snapkit
+ - redo frames with snapkit
+ 
+ Other
+ - remove root controller nav bar 
+ 
+ 
+ 
+ 
+ 
+ */
