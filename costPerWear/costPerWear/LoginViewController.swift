34cc330b0b4b3f6e5b0183be1fb5ebce4e2e9c81
tameika <tameika.lawrence@gmail.com>
2017-08-17 01:58:08 -0400

    loginVC added

---
+++ costPerWear/costPerWear/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 8/17/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AWSCognito
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
