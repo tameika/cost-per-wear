66fae5971a55d99b39c2fd3bf701f96484ed7066
tameika <tameika.lawrence@gmail.com>
2017-08-18 02:09:00 -0400

    created auth file

---
+++ costPerWear/costPerWear/AWSAuth.swift
@@ -0,0 +1,13 @@
+//
+//  AWSAuth.swift
+//  costPerWear
+//
+//  Created by Tameika Lawrence on 8/18/17.
+//  Copyright © 2017 Tameika Lawrence. All rights reserved.
+//
+
+import Foundation
+import AWSCognito
+import AWSCognitoIdentityProvider
+
+
