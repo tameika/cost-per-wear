fed94fd4edfe24964301e8256849f51f28291419
tameika <tameika.lawrence@gmail.com>
2017-08-18 03:15:27 -0400

    setting up custom auth server

--- costPerWear/costPerWear/AWSAuth.swift
+++ costPerWear/costPerWear/AWSAuth.swift
@@ -7,7 +7,66 @@
 //
 
 import Foundation
+import AWSCore
 import AWSCognito
 import AWSCognitoIdentityProvider
+
+// MARK : To make my own custom authentication server
+
+
+class CPWIdentityProvider: AWSAbstractCognitoCredentialsProvider {
+    
+    var _token: String!
+    var _logins: [NSObject : AnyObject]!
+    
+     override var token: String {
+        
+        get {
+            return _token
+        }
+    }
+    
+    
+    override var logins: [NSObject : AnyObject] {
+        
+        get {
+            return _logins
+        }
+        
+        set {
+            
+            _logins = newValue
+        }
+    }
+    
+    
+    override func getIdentityId() -> AWSTask! {
+        
+        if self.identityId != nil {
+            return AWSTask(result: self.identityId)
+        } else {
+            return AWSTask(result: nil).continueWithBlock({ (task) -> AnyObject! in
+                if self.identityId == nil {
+                    return self.refresh()
+                }
+                return AWSTask(result: self.identityId)
+            })
+        
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
