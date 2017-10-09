8a7fbc8d83f8f0e8d5a3f769d4bd9d97fb61dc5d
tameika <tameika.lawrence@gmail.com>
2017-10-08 21:51:18 -0400

    delegates moved

--- costPerWear/costPerWear/CameraViewController.swift
+++ costPerWear/costPerWear/CameraViewController.swift
@@ -11,7 +11,6 @@ import AVFoundation
 
 class CameraViewController: UIViewController {
     
-    var camera = Camera()
     
     //var imagePicked: UIImageView!
     
@@ -25,51 +24,4 @@ class CameraViewController: UIViewController {
 
    
 }
-
-extension CameraViewController: CameraViewDelegate {
-    
-    func onTapTakePhoto() {
-        
-        var output = camera.capturePhotoOutput
-        
-         // Make sure capturePhotoOutput is valid
-        
-        guard let capturePhotoOutput = output else { return }
-        
-         // Get an instance of AVCapturePhotoSettings class
-        
-        let photoSettings = AVCapturePhotoSettings()
-        photoSettings.isAutoStillImageStabilizationEnabled = true
-        photoSettings.isHighResolutionPhotoEnabled = true
-        photoSettings.flashMode = .auto
-        
-        // Call capturePhoto method by passing our photo settings and a delegate implementing AVCapturePhotoCaptureDelegate
-        
-        output?.capturePhoto(with: photoSettings, delegate: self)
-        
-    }
-}
-
-extension CameraViewController: AVCapturePhotoCaptureDelegate {
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
-        
-         // Make sure we get some photo sample buffer
-        
-        guard error == nil, let photoSampleBuffer = photoSampleBuffer else { print("Error capturing photo: \(String(describing: error))"); return }
-        
-        
-        // Convert photo same buffer to a jpeg image data by using AVCapturePhotoOutput
-        
-        guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else { return }
-    
-    // initialize a UIImage with our image data
-        
-    let capturedImage = UIImage.init(data: image, scale: 1.0)
-    guard let image = capturedImage else { return }
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-}
-
-}
-
 
